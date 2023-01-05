1ee3b28734570113aea10448e6438ee4c50bdd17
Logan <73564929+larrythegnome@users.noreply.github.com>
2023-01-05 13:20:21 -0500

    google

--- API_3/Google.py
+++ API_3/Google.py
@@ -1,4 +1,13 @@
 import requests
 
-query = (input("Enter the book you'd like to search"))
+endpoint = "https://www.googleapis.com/books/v1/volumes"
+query = input("Enter the book you’d like to search ")
 
+params = {"q": query, "maxResults": 3}
+response = requests.get(endpoint, params=params).json()
+for book in response["items"]:
+    volume = book["volumeInfo"]
+    title = volume["title"]
+    published = volume["publishedDate"]
+    description = volume["description"]
+    print(f"{title} ({published}) | {description}")
